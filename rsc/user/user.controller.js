af34a5ea96f2d95a9b9a54ad2eff6111499f8168
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-02-13 15:35:20 +0100

    backend start

--- rsc/user/user.controller.js
+++ rsc/user/user.controller.js
@@ -6,8 +6,8 @@ async function getUsers(req, res, err) {
 }
 
 async function getUserById(req, res, err) {
-    const diary = await UserModel.create(req.body)
-    res.status(201).json(diary)
+    const user = await UserModel.findById(req.params.id)
+    res.status(201).json(user)
 }
 
 async function registerUser(req, res, err) {
@@ -16,13 +16,18 @@ async function registerUser(req, res, err) {
 }
 
 async function logIn(req, res, err) {
-    const diary = await UserModel.find({username: req.body.username})
+    try {
+        const diary = await UserModel.find({username: req.body.username})
+        req.session = diary
     res.status(200).json(diary)
+    } catch (error) {
+        res.status(404).json(error)
+    }
 }
 
 async function logOut(req, res, err) {
-    const diary = await UserModel.create(req.body)
-    res.status(201).json(diary)
+    req.session = null
+    res.status(200).json("logged out")
 }
 
 module.exports = {getUsers, getUserById, registerUser, logIn, logOut}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cookieSession = require("cookie-session")
+app.use(
+    cookieSession({
+        secret: "secret",
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24,
+    })
+);
 
 const { userRoute } = require("./rsc/user/user.router")
 const { diaryRoute } = require("./rsc/diary/diary.router")
@@ -13,7 +22,7 @@ app.use("/diary", diaryRoute)
 async function init() {
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect('mongodb+srv://sarapellnor:R77zPxx1QYMUvu3U@cluster0.hbp4b1l.mongodb.net/test');
+        await mongoose.connect('mongodb+srv://sarapellnor:R77zPxx1QYMUvu3U@cluster0.hbp4b1l.mongodb.net/Diary');
         app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
     } catch (err) {
         console.error(err);
